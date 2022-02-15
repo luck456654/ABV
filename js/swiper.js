a17ee006dbef4c23e04d970fb6d95c7e933705b8
Vadim <luckjantsev@rambler.ru>
2022-02-15 10:11:16 +0300

    15022022

---
+++ js/swiper.js
@@ -0,0 +1,123 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-nextt',
+      prevEl: '.swiper-button-prevv',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  const swiper2 = new Swiper('.swiper2', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-nexts',
+      prevEl: '.swiper-button-prevs',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  const swiper3 = new Swiper('.swiper3', {
+   
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.print__pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-nextr',
+      prevEl: '.swiper-button-prevr',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  const swiperadv = new Swiper('.swiperadv', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-nextr',
+      prevEl: '.swiper-button-prevr',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  const swiperslidermobile = new Swiper('.swiperslidermobile', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-nextr',
+      prevEl: '.swiper-button-prevr',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  const swiperpartners = new Swiper('.swiperpartners', {
+    slidesPerView: 3,
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-paginationpartners',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-nextr',
+      prevEl: '.swiper-button-prevr',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  

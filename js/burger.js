94297f2b50cb068ecd2d7b7fe02c789cc41919f7
Vadim <luckjantsev@rambler.ru>
2022-03-01 14:18:11 +0300

    01032022

--- js/burger.js
+++ js/burger.js
@@ -1,23 +1,34 @@
-let burger=document.querySelector(".header__burger");
-let nav=document.querySelector(".print__nav");
-burger.addEventListener("click",openburger);
-document.addEventListener("resize",openburger);
-let i=0;
-
-function openburger(){
-   
-    if(i==0){
-    nav.style.transform="translateX(509px)";
-    nav.style.transition="transform 2s linear";
-    burger.src="img/burger-close.png";
-    i=1;
-    }
-    else{
-        burger.src="img/burger-icon.png";
-        i=0;
-    nav.style.transform="translateX(-509px)";
-    nav.style.transition="transform 2s linear";    
-    }
-}
-
-
+function setBurger(params) {
+    const btn = document.querySelector(`.${params.btnClass}`);
+    const menu = document.querySelector(`.${params.menuClass}`);
+  
+    menu.addEventListener("animationend", function () {
+      if (this.classList.contains(params.hiddenClass)) {
+        this.classList.remove(params.activeClass);
+        this.classList.remove(params.hiddenClass);
+      }
+    });
+  
+    btn.addEventListener("click", function () {
+      this.classList.toggle(params.activeClass);
+  
+      if (
+        !menu.classList.contains(params.activeClass) &&
+        !menu.classList.contains(params.hiddenClass)
+      ) {
+        menu.classList.add(params.activeClass);
+        document.body.style.overflow = 'hidden';
+      } else {
+        menu.classList.add(params.hiddenClass);
+        document.body.removeAttribute('style');
+      }
+    });
+  }
+  
+  // здесь мы вызываем функцию и передаем в нее классы наших элементов
+  setBurger({
+    btnClass: "js-burger", // класс бургера
+    menuClass: "js-menu-wrap", // класс меню
+    activeClass: "is-opened", // класс открытого состояния
+    hiddenClass: "is-closed" // класс закрывающегося состояния (удаляется сразу после закрытия)
+  });
---
+++ js/gallery.js
@@ -0,0 +1,106 @@
+const galleryChoicesSelect=document.querySelector('.js-gallery-choices')
+const galleryChoices=new Choices(galleryChoicesSelect,{
+  shouldSort:false,
+  searchEnabled:false,
+  itemSelectText:'',
+  position:'bottom',
+  classNames: {
+    containerOuter: 'filter-choices gallery__choices',
+    containerInner: 'filter-choices__inner',
+    input: 'filter-choices__input',
+    inputCloned: 'filter-choices__input--cloned',
+    list: 'filter-choices__list',
+    listItems: 'filter-choices__list--multiple',
+    listSingle: 'filter-choices__list--single',
+    listDropdown: 'filter-choices__list--dropdown',
+    item: 'filter-choices__item',
+    itemSelectable: 'filter-choices__item--selectable',
+    itemDisabled: 'filter-choices__item--disabled',
+    itemChoice: 'filter-choices__item--choice',
+    placeholder: 'filter-choices__placeholder',
+    group: 'filter-choices__group',
+    groupHeading: 'filter-choices__heading',
+    button: 'filter-choices__button',
+    activeState: 'is-active',
+    focusState: 'is-focused',
+    openState: 'is-open',
+    disabledState: 'is-disabled',
+    highlightedState: 'is-highlighted',
+    selectedState: 'is-selected',
+    flippedState: 'is-flipped',
+    loadingState: 'is-loading',
+    noResults: 'has-no-results',
+    noChoices: 'has-no-choices'
+  },
+});
+
+
+
+
+
+let gallerySlider = new Swiper(".js-gallery-slider", {
+  slidesPerView: 1,
+  grid: {
+    rows: 1,
+    fill: "row"
+  },
+  spaceBetween: 20,
+  pagination: {
+    el: ".js-gallery-pagination",
+    type: "fraction"
+  },
+  navigation: {
+    nextEl: ".gallery__btn-next",
+    disabledClass:"nav-btn--disabled",
+    prevEl: ".gallery__btn-prev"
+  },
+
+  breakpoints: {
+    421: {
+      slidesPerView: 2,
+      grid: {
+        rows: 2
+      },
+      spaceBetween: 30
+    },
+
+    1281: {
+      slidesPerView: 3,
+      grid: {
+        rows: 2
+      },
+      spaceBetween: 50
+    }
+  },
+  
+  a11y: false,
+  keyboard: true, // можно управлять с клавиатуры стрелками влево/вправо
+  
+  // Дальнейшие надстройки делают слайды вне области видимости не фокусируемыми 
+  watchSlidesProgress: true,
+  watchSlidesVisibility: true,
+  slideVisibleClass: 'slide-visible',
+  
+  on: {
+    init: function () {
+      this.slides.forEach(slide => {
+        if (!slide.classList.contains('slide-visible')) {
+          slide.tabIndex = '-1';
+        } else {
+          slide.tabIndex = '';
+        }
+      });
+    },
+    slideChange: function () {
+      this.slides.forEach(slide => {
+        if (!slide.classList.contains('slide-visible')) {
+          slide.tabIndex = '-1';
+        } else {
+          slide.tabIndex = '';
+        }
+      });
+    }
+  }
+
+ 
+});
